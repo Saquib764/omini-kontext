46cdf33add8c534e9b521823ce332085c75101e0
Saquiub <saquibvswild@gmail.com>
2025-08-08 17:09:35 +0530

    Add OminiKontextModelPatch class for model patching functionality; update conditioning method to handle new latent structure with delta parameters for improved model conditioning.

--- comfyui_nodes/omini_kontext.py
+++ comfyui_nodes/omini_kontext.py
@@ -1,5 +1,21 @@
 import node_helpers
 
+class OminiKontextModelPatch:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {"required": { "model": ("MODEL", ),
+                             }}
+    RETURN_TYPES = ("MODEL",)
+    FUNCTION = "apply_patch"
+
+    CATEGORY = "advanced/conditioning/edit_models"
+
+    def apply_patch(self, model):
+        print(model)
+        print(model.model)
+        print(model.model.diffusion_model)
+        return (model,)
+
 
 class OminiKontextConditioning:
     @classmethod
@@ -19,11 +35,8 @@ class OminiKontextConditioning:
     DESCRIPTION = "This node sets the reference latent for Flux Kontext model. By default, the model doesn't support two images as input, so this model requires a LoRA trained with omini-kontext framework."
 
     def append(self, conditioning, latent=None, delta_0=0, delta_1=0, delta_2=0):
-        print(latent)
-        print(latent["samples"])
-        print(latent["batch_index"])
         if latent is not None:
-            conditioning = node_helpers.conditioning_set_values(conditioning, {"reference_latents": [latent["samples"]]}, append=True)
+            conditioning = node_helpers.conditioning_set_values(conditioning, {"omini_latents": [{"latent":latent["samples"]}], "delta": [delta_0, delta_1, delta_2]}, append=True)
         return (conditioning, )
 
 
